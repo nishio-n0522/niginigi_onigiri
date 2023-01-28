511f3953ce4a7d15f60f28e4a70826d12cbd5f5e
nozomu <nishio.n0522@gmail.com>
2023-01-28 15:00:59 +0900

    add press_rice_into_a_ball method in rice.py

--- in_python/rice.py
+++ in_python/rice.py
@@ -3,6 +3,14 @@ class RiceType:
     Akitakomachi: str = "nice"
     Yumepirika: str = "cute"
 
+class OnigiriContents:
+    Umeboshi: str = "very_sour"
+
+class RiceBall:
+    def __init__(self, rice_type: RiceType, contents: OnigiriContents = None) -> None:
+        self.rice_type = rice_type
+        self.contents = contents
+
 class Rice:
     def __init__(self, rice_type: RiceType) -> None:
         self.rice_type = rice_type
@@ -14,3 +22,6 @@ class Rice:
 
     def boil_rice(self) -> None:
         self.is_boiled = True
+
+    def press_rice_into_a_ball(self) -> RiceBall:
+        return RiceBall(self.rice_type)
